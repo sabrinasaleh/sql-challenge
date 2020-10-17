b55b654a3cdf4b60d9b95ae31acfb4d67160796f
sabrinasaleh <sabrinafsaleh@gmail.com>
2020-10-17 05:32:52 +0000

    added queries for epilogue

---
+++ Employee_SQL_Other/epilogue.sql
@@ -0,0 +1,12 @@
+-- Epilogue 
+SELECT employees.emp_no, titles.title, departments.dept_name, employees.first_name, employees.last_name, employees.sex, salaries.salary
+FROM employees
+JOIN titles
+ON employees.emp_title_id = titles.title_id
+JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+JOIN salaries
+ON employees.emp_no = salaries.emp_no 
+WHERE employees.emp_no = 499942;
